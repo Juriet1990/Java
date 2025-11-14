c3cde435181dad9992d90886a05198f341a73407
Juriet <giulia.dimattei@gmail.com>
2025-11-14 18:29:01 +0100

    Delete Giulia_DiMattei_Java_Data directory

--- Giulia_DiMattei_Java_Data/08_07_2025/src/exceptions/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class UserAlreadyExistsException extends Exception{ // ereditarietà classe exception
-
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    } // chiama la super classe e passa il messaggio
-}
--- Giulia_DiMattei_Java_Data/08_07_2025/src/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class UserNotFoundException extends Exception{ // come l'altra exception
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
