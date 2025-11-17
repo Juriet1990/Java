bb2bab4605dd59acbf996701925f938b297c9170
Giulia <giulia.dimattei@gmail.com>
2025-11-17 18:00:47 +0100

    Update FilmController.java

--- Allenamento-EsameJava/FilmController.java
+++ Allenamento-EsameJava/FilmController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.sql.SQLException;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping ("api/film/")
 public class FilmController {
