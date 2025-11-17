764ecdde2a9cc83492c1eaac7a464d467b6168a5
Juriet <giulia.dimattei@gmail.com>
2025-11-17 12:30:03 +0100

    Update DBConfig.java
    PPW è un modo vecchio, meglio usare PWD

--- Allenamento-EsameJava/DBConfig.java
+++ Allenamento-EsameJava/DBConfig.java
@@ -3,18 +3,18 @@ package.cinema.constants;
 public class DBConfig {
     public static final String URL = "jdbc:mysql://localhost:3306/cinema";
     public static final String USER = "root";
-    public static final String PPW = "root";
+    public static final String PWD = "root";
 }
 
 // Esercizi
 public class DBConfig {
     public static final String URL = "jdbc:mysql://localhost:3306/cinema";
     public static final String USER = "root";
-    public static final String PPW = "root";
+    public static final String PWD = "root";
 }
 
 public class DBConfig {
     public static final String URL = "jdbc:mysql://localhost:3306/cinema";
     public static final String USER = "root";
-    public static final String PPW = "root";
+    public static final String PWD = "root";
 }
