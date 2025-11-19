24e35faaa7359f23a1780f2a9462cda73a36afc6
Giulia <giulia.dimattei@gmail.com>
2025-11-19 15:51:41 +0100

    Delete spedizioni directory
    Spostata di Repository

--- spedizioni/src/main/java/com/spedizioni/constants/DBConfig.java
+++
@@ -1,7 +0,0 @@
-package com.spedizioni.constants;
-
-public class DBConfig {
-    public static final String URL = "jdbc:mysql://localhost:3306/spedizioni";
-    public static final String USER = "root";
-    public static final String PWD = "root";
-}
